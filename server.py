31da9a333a94037255095ad8fa885b4ab68c8b2e
PedrolitoLsg <84906663+PedrolitoLsg@users.noreply.github.com>
2022-02-21 18:28:23 +0100

    2Adding Virtual Environnment

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask,render_template,request,redirect,flash,url_for
+from flask import Flask, render_template, request, redirect, flash, url_for
 
 
 def loadClubs():
